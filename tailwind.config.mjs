a96abaf4cd9e7e4e94ab47d33c6f89bdb8c0733f
Dani-Bcn <nneodani@gmail.com>
2023-12-30 18:10:02 +0100

    test mobile

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		fontFamily: {
-			'dancing': ['"Dancing Script"'],
-			'bad': ['"Bad Script"'],
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      dancing: ['"Dancing Script"'],
+      bad: ['"Bad Script"'],
+      Righteous: ['"Righteous"'],
+    },
+  },
+  plugins: [],
+};
